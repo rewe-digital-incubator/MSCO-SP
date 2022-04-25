998f78665966e7c5184100d5688306062dafc6ea
Marc Kalmes <marc.kalmes@rewe-digital.com>
2022-04-25 10:17:04 +0200

    Fix URL

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSCO",
-            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.1.0-alpha.1/MSCO.xcframework.zip",
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.1-alpha.1/MSCO.xcframework.zip",
             checksum: "893dbce281060298215c39423ef34694a433f94cc1660c9aed251f0fbc52caf3")
     ]
 )
