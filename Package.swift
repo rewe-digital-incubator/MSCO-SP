39802b953ca3903ec9799e855dc21bf79e554e09
rd-marc-gidaszewski <104762823+rd-marc-gidaszewski@users.noreply.github.com>
2023-01-16 13:32:16 +0100

    update package manifest

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "ShopremeCore",
+    name: "MSCO",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(
-            name: "ShopremeCore",
-            targets: ["ShopremeCore"]),
+            name: "MSCO",
+            targets: ["MSCO"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "ShopremeCore",
-            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.1/ShopremeCore.xcframework.zip",
-            checksum: "69859c12bf220cda038ff757ef2f862b01eb0cacdf7e7e42e5cb741df33fa34f")
+            name: "MSCO",
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.2-alpha/MSCO.xcframework.zip",
+            checksum: "4f272ae2f7a67fdf52d847f7f4a6057804c086ad994ce95c44bafaad3afbd9bd")
     ]
 )
