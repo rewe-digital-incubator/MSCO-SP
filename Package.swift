fed0bd4d8e94a53ac1b9fa388828629c2cbd5814
Marc Kalmes <155171906+rd-marc-kalmes@users.noreply.github.com>
2024-05-14 13:16:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSCO",
-            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.9-beta.3/MSCO.xcframework.zip",
-            checksum: "ec0bb4babd661e868589408a689d9a1609b684217ba98aa309de8a957d28596d")
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.9.0/MSCO.xcframework.zip",
+            checksum: "c01700bc517958c076182aca2cea64dcd888c11a24e2a13a243307cd98fb41ff")
     ]
 )
