8386d8bb91e2a5b5c6dfc9bca9e37780b5f370aa
MSCO Build Service <>
2024-12-03 19:22:58 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,24 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "MSCO",
     platforms: [
-      .iOS(.v15)
+        .iOS(.v15)
     ],
     products: [
-      .library(
-        name: "MSCO",
-        targets: ["MSCO"]
-      ),
+        .library(
+            name: "MSCO",
+            targets: ["MSCO"]
+        ),
     ],
-    dependencies: [],
+    dependencies: [ ],
     targets: [
-      .binaryTarget(
-        name: "MSCO",
-        url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.11.1/MSCO.xcframework.zip",
-        checksum: "1cf97fbc117242642b7d9c072c4db742d1bf72c7a2de4469959c04a63e335521"
-      )
+        .binaryTarget(
+            name: "MSCO",
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v0.12.0/MSCO.xcframework.zip",
+            checksum: "a47ea9b76715ed915d05da62d4f8d65b656bc8825c294d024f5ed6b05d3fbe27"
+        )
     ]
   )
