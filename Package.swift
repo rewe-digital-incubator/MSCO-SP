ffc6b8e03bc9625564398ed9be4bdf787a07482b
MSCO Build Service <>
2025-08-21 16:16:07 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSCO",
-            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v2.2.0/MSCO.xcframework.zip",
-            checksum: "bb9ed1d8bf55338c87467154e319a81b30c30a88e98b8a35d1bec9c3e77584c8"
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v2.2.1/MSCO.xcframework.zip",
+            checksum: "106cf758cd8039740616ec2db01b4ae49ba462e506b660fbabadf20d8aba8ce3"
         )
     ]
   )
