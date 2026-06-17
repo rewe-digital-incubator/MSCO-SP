e6b83c9ffbf6016c631d9f0c4200b19afcfc3d3d
MSCO Build Service <>
2026-06-17 10:03:58 +0000

    Release v4.0.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MSCO",
     platforms: [
-        .iOS("17.0")
+        .iOS("18.0")
     ],
     products: [
         .library(
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MSCO",
-            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v3.4.2/MSCO.xcframework.zip",
-            checksum: "3a5c8c4d6870f298db7d26134b1235258b57fabbe6a90832423ce9bb537ae437"
+            url: "https://github.com/rewe-digital-incubator/MSCO-SP/releases/download/v4.0.0/MSCO.xcframework.zip",
+            checksum: "01e9f3259010159a3703974f3c034c27c186d1f886f1f97484116d3c952bfece"
         )
     ]
 )
